d5e172eeaf87026705d7f1ca793a8e1c5a941119
manyfutyu <armando.erbana07@gmail.com>
2025-07-19 20:35:54 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <div class="d-flex justify-content-center gap-3 mt-4 flex-wrap">
         <a href="{{ route('employees.index') }}" class="btn btn-primary">Karyawan</a>
         <a href="{{ route('departments.index') }}" class="btn btn-secondary">Departemen</a>
-        <a href="{{ route('attendance.ketepatan') }}" class="btn btn-info">Cek Ketepatan Absensi</a>
+        <a href="{{ route('attendance.ketepatan') }}" class="btn btn-info">History kehadiran</a>
         <a href="{{ route('attendance.index') }}" class="btn btn-dark">Data Absensi</a>
     </div>
 </div>
